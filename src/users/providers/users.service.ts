45cf93cd98c9622cfa97f0cfab5cd7b239e805b6
Swapnil Sarkar <swapnilmkab@gmail.com>
2024-10-08 18:56:19 +0530

    transferred business logic to user services

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from '../dtos/createUser.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public usersArray: CreateUserDto[] = [];
+
+  public checkUserExists(id: number, email: string): CreateUserDto {
+    const foundUser = this.usersArray.find(
+      (user: CreateUserDto) => user.id === id || user.email === email,
+    );
+    return foundUser;
+  }
+
+  public findUsers(limit: number, page: number, id?: number) {
+    console.log('limit : ', limit);
+    console.log('page :', page);
+    if (id) {
+      const findUser = this.usersArray.find(
+        (user: CreateUserDto) => user.id === id,
+      );
+      if (findUser) return findUser;
+      else throw new NotFoundException('No user with the given id found');
+    } else return this.usersArray;
+  }
+
+  public createUser(createUserDto: CreateUserDto) {
+    const foundUser = this.checkUserExists(
+      createUserDto.id,
+      createUserDto.email,
+    );
+    if (foundUser) {
+      throw new BadRequestException('User already exists');
+    }
+    this.usersArray.push(createUserDto);
+    return createUserDto;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,17 +11,11 @@ import {
 } from '@nestjs/common';
 import { GetUsersParamsDto } from './dtos/getUserParams.dto';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
-  public usersArray: CreateUserDto[] = [];
-
-  public checkUserExists(id: number, email: string): CreateUserDto {
-    const foundUser = this.usersArray.find(
-      (user: CreateUserDto) => user.id === id || user.email === email,
-    );
-    return foundUser;
-  }
+  constructor(private readonly usersService: UsersService) {}
 
   @Get('/:id?')
   public getUsers(
@@ -29,22 +23,11 @@ export class UsersController {
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log('params : ', getUsersParamDto);
-    console.log('limit : ', limit);
-    console.log('page :', page);
-    return this.usersArray;
+    return this.usersService.findUsers(limit, page, getUsersParamDto.id);
   }
 
   @Post('/create')
   public postUsers(@Body() createUserDto: CreateUserDto) {
-    const foundUser = this.checkUserExists(
-      createUserDto.id,
-      createUserDto.email,
-    );
-    if (foundUser) {
-      throw new BadRequestException('User already exists');
-    }
-    this.usersArray.push(createUserDto);
-    return createUserDto;
+    return this.usersService.createUser(createUserDto);
   }
 }
