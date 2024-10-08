f37701d75973f098b5bb9311cbdbd0c657d23eac
Swapnil Sarkar <swapnilmkab@gmail.com>
2024-10-08 21:45:59 +0530

    Connected posts module with users module (inter-modular dependency)

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreatePostDto } from './dtos/createPosts.dto';
+import { PostsService } from './providers/posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post('/create')
+  public createPost(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.createPost(createPostDto);
+  }
+}
--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
+import { CreatePostDto } from '../dtos/createPosts.dto';
+import { CreateUserDto } from 'src/users/dtos/createUser.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly usersService: UsersService) {}
+
+  public createPost(createPostDto: CreatePostDto) {
+    const latestUser = this.usersService.usersArray.find(
+      (user: CreateUserDto) => user.id === 1,
+    );
+    return { ...createPostDto, user: latestUser.firstName };
+  }
+}
